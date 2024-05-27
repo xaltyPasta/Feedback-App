9c3f44bbd64c59207a299867b878027921cabaa4
xaltyPasta <BTECH10188.21@bitmesra.ac.in>
2024-05-28 04:17:20 +0530

    Email Verification Added

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import {resend} from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    verifyCode:string
+): Promise<ApiResponse> {
+    try{
+        await resend.emails.send({
+            from:'you@example.com',
+            to:email,
+            subject:'True Feedback verification code',
+            react:VerificationEmail({username,otp:verifyCode}),
+        })
+        return {success:true,message:'Verification Email sent successfully'}
+    }catch(emailError){
+        console.error("Error Sending verification email",emailError)
+        return {success:false,message:'Failed to send verification email'}
+
+    }
+    
+}
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,20 +1,25 @@
-import mongoose, { connection } from "mongoose";
+import mongoose from "mongoose";
 
 type ConnectionObject={
     isConnected?:Number
 }
 
-const connection:ConnectionObject={
-
-}
+const connection:ConnectionObject={}
 
 async function dbConnect():Promise<void> {
     if(connection.isConnected){
         console.log("Already connected to the database");
-        return;
+        return
     }
     try{
-        await
+        const db =await mongoose.connect(process.env.MONGODB_URI || '',{});
+        connection.isConnected=db.connections[0].readyState;
+        console.log("DB Connected Successfully");
+    
+    }catch (error){
+        console.log("DB Connection Failed",error);
+        process.exit(1);
     }
     
 }
+export default dbConnect;
